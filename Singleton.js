322d9966a58a66b4b24863f01c6448dbdcf2055c
WallaceRepo <47252820+WallaceRepo@users.noreply.github.com>
2023-01-04 12:39:02 -0600

    Create Singleton.js

---
+++ Singleton.js
@@ -0,0 +1,71 @@
+// Sinlgeton is the way of creating Single Object that is shared among bunch other objects throughout my app, without having to recreate it. All the states, methods, variables of that object is shared among other objects, they don't create their version of instance, just uses the existing one.
+
+class FancyLogger {
+    constructor() {
+        if (FancyLogger.instance == null) {
+              this.logs = []
+              FancyLogger.instance = this
+        }
+        return FancyLogger.instance
+    }
+log(message) {
+    this.logs.push(message)
+    console.log(`Fancy:  ${message}`)
+}
+printLogCount() {
+    console.log(` ${this.logs.length} Logs`)
+}
+}
+
+const logger = new FancyLogger();
+Object.freeze(logger);
+
+
+logger.printLogCount();
+logger.log('Second File')
+logger.log('Third File')
+logger.log('Ford File')
+logger.printLogCount()
+console.log('---------')
+
+class Logger {
+    constructor() {
+       this.logs = []
+    }
+log(message) {
+    this.logs.push(message)
+    console.log(`Fancy:  ${message}`)
+}
+printLogCount() {
+    console.log(` ${this.logs.length} Logs`)
+ }
+}
+
+const logger1 = new Logger();
+const logger2 = new Logger();
+const logger3 = new Logger();
+
+logger.printLogCount();
+logger1.log('Second File')
+logger2.log('Third File')
+logger3.log('Ford File')
+
+logger2.printLogCount()
+logger3.printLogCount()
+logger1.printLogCount()
+
+/* 
+0 Logs
+Fancy:  Second File
+Fancy:  Third File
+Fancy:  Ford File
+ 3 Logs
+---------
+3 Logs
+Fancy:  Second File
+Fancy:  Third File
+Fancy:  Ford File
+ 1 Logs
+ 1 Logs
+ 1 Logs
+*/
