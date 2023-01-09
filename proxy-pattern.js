4dbe618dfd2790de0fe7a96705e8483496539852
WallaceRepo <47252820+WallaceRepo@users.noreply.github.com>
2023-01-08 22:07:15 -0600

    Create proxy-pattern.js

---
+++ proxy-pattern.js
@@ -0,0 +1,5 @@
+// proxy pattern: Object provides surrogate / placeholder
+// for another object to control access to it.
+// So interacting with object through proxy object instead of directly. 
+// proxy means delegate, represantative, surrogate etc
+// Usage: Interacting Remote resource, Virtual proxy controls access to resource that is expensive to create; cashing etc, Protection proxy is access management.
